7ca232f111002c608c708066b36fdb2e854a79c8
VishnuSai1618 <kvishnusai5240574@gmail.com>
2025-08-04 11:34:35 +0530

    update at aug 4

--- flashcard_project/quiz/forms.py
+++ flashcard_project/quiz/forms.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
+    # Add the first_name field
+    first_name = forms.CharField(max_length=150, required=True, help_text='Please enter your full name.')
+
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'email')
+        # Add first_name to the list of fields
+        fields = ('first_name', 'username', 'email')
+
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        # Save the first_name from the form
+        user.first_name = self.cleaned_data["first_name"]
+        if commit:
+            user.save()
+        return user
--- flashcard_project/quiz/serializers.py
+++ flashcard_project/quiz/serializers.py
@@ -6,20 +6,28 @@ from .models import Deck, Question, Choice
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
-        fields = ['id', 'choice_text', 'is_correct']
+        # Add 'question' to the fields list
+        fields = ['id', 'question', 'choice_text', 'is_correct']
+        # 'question' is only needed when creating a choice, not when reading it.
+        extra_kwargs = {'question': {'write_only': True}}
 
 class QuestionSerializer(serializers.ModelSerializer):
+    # This correctly shows the choices when we read a question
     choices = ChoiceSerializer(many=True, read_only=True)
 
     class Meta:
         model = Question
-        fields = ['id', 'question_text', 'question_type', 'choices']
+        # Add 'deck' to the fields list
+        fields = ['id', 'deck', 'question_text', 'question_type', 'choices']
+        # 'deck' is only needed when creating a question, not when reading it.
+        extra_kwargs = {'deck': {'write_only': True}}
+
 
 class DeckSerializer(serializers.ModelSerializer):
-    # 'questions' is the related_name we set in the Question model
+    # This correctly shows the questions when we read a deck
     questions = QuestionSerializer(many=True, read_only=True)
 
     class Meta:
         model = Deck
         fields = ['id', 'name', 'description', 'activity_type', 'user', 'questions']
-        read_only_fields = ['user'] # User will be set automatically from the request
+        read_only_fields = ['user']
